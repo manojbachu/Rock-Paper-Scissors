51b16b9240c3aa90f8dda33c2c5dd6f7c3f53fc1
manojbachu <bachumanoj2001@gmail.com>
2023-10-12 14:20:39 +0000

    progress

---
+++ src/components/GameOptions/styledComponents.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const OptionListItem = styled.li`
+  list-style-type: none;
+`
+
+export const OptionImage = styled.img`
+  width: 170px;
+  height: 70px;
+`
+
+export const OptionListImage = styled.li`
+  list-style-type: none;
+  display: flex;
+`
+
+export const GameOptionButton = styled.button`
+  outline: none;
+  border: none;
+  background: none;
+  cursor: pointer;
+`
